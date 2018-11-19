f2ab558a6f8514eb37e919e99e31512de53e5cf0
Kristoffer Henden Kaasen <kristhka@stud.ntnu.no>
2018-11-19 22:53:22 +0100

    Fixed main, (last merge did it wrong)

--- Node1/main.c
+++ Node1/main.c
@@ -1,7 +1,53 @@
+#define F_CPU 4915200UL
 
-
-#include "fsm.h"
-
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
 #include <avr/interrupt.h>
 
+#include "uart.h"
+#include "CAN_driver.h"
+#include "SPI_driver.h"
+#include "MCP2515_driver.h"
+#include "sram.h"
+#include "joystick.h"
+#include "buttons.h"
+#include "sliders.h"
+#include "oled.h"
+#include "menu.h"
+#include "SPI_driver.h"
+#include "MCP2515_driver.h"
+#include "CAN_driver.h"
+#include "MCP2515.h"
+#include "game.h"
+#include "fsm.h"
+#include "EEPROM_driver.h"
+
+#define Baudrate 9600
+#define MYUBRR F_CPU/16/Baudrate-1
+
+//Cutoff frequency: 796.18 Hz
+//ttyS0 putty
+
 void init_all()
+{
+  	cli();
+
+    UART_init(MYUBRR);
+    SRAM_init();
+    oled_reset();
+    multi_card_init();
+    menu_init();
+    CAN_init();
+    fsm_init();
+
+    sei();
+}
+
+void main()
+{    
+  init_all();
+
+  fsm();
+}
